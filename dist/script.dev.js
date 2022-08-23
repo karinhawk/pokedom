49f5047838212f9090462cbf6b0226f7905c59ba
Karin Hawksworth <karin.hawksworth@gmail.com>
2022-08-23 22:27:11 +0100

    added search bar

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -3,6 +3,7 @@
 var _pokemon = require("./data/pokemon.js");
 
 console.log(_pokemon.pokemonArray);
+var searchResult = [];
 
 var getPokemon = function getPokemon(pokemon) {
   return "<div class=\"card\">\n            <div class=\"card__content\">".concat(pokemon.id, "</div>\n            <img class=\"card__content card__image\" src=\"").concat(pokemon.sprite, "\"/>\n            <h1 class=\"card__content card__heading card__text\">").concat(pokemon.name, "</h1>\n            <div class=\"card__content\"><p class=\"card__content card__text\">is a ").concat(pokemon.types.join(", "), " type pokemon</p></div>\n        </div>");
@@ -13,3 +14,25 @@ var card = document.querySelector(".card-container");
 _pokemon.pokemonArray.forEach(function (pokemon) {
   card.innerHTML += getPokemon(pokemon);
 });
+
+var searchBar = document.getElementsByTagName("input")[0];
+
+var searchPokemon = function searchPokemon(event) {
+  event.preventDefault();
+  var input = event.target.value.toLowerCase();
+  console.log(input);
+  searchResult = _pokemon.pokemonArray.filter(function (pokemon) {
+    return pokemon.name.includes(input);
+  });
+  console.log(searchResult);
+};
+
+var showResult = function showResult() {
+  card.innerHTML = "";
+  searchResult.forEach(function (result) {
+    card.innerHTML += getPokemon(result);
+  });
+};
+
+searchBar.addEventListener("keyup", searchPokemon);
+searchBar.addEventListener("keyup", showResult);
--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-import {pokemonArray} from "./data/pokemon.js";
+import {
+    pokemonArray
+} from "./data/pokemon.js";
 console.log(pokemonArray);
+
+let searchResult = [];
 
 const getPokemon = (pokemon) => {
     return `<div class="card">
             <div class="card__content">${pokemon.id}</div>
             <img class="card__content card__image" src="${pokemon.sprite}"/>
             <h1 class="card__content card__heading card__text">${pokemon.name}</h1>
-            <div class="card__content"><p class="card__content card__text">is a ${pokemon.types.join(", ")} type pokemon</p></div>
-        </div>`
-}
-
+            <div class="card__content"><p class="card__content card__text">is a ${pokemon.types.join(
+            ", "
+        )} type pokemon</p></div>
+        </div>`;
+};
 const card = document.querySelector(".card-container");
 
 pokemonArray.forEach((pokemon) => {
     card.innerHTML += getPokemon(pokemon);
-   });
+});
+
+const searchBar = document.getElementsByTagName("input")[0];
+
+const searchPokemon = (event) => {
+    event.preventDefault();
+    const input = event.target.value.toLowerCase();
+    console.log(input);
+    searchResult = pokemonArray.filter(pokemon => pokemon.name.includes(input));
+    console.log(searchResult);
+};
+
+const showResult = () => {
+    card.innerHTML = "";
+    searchResult.forEach((result) => {
+        card.innerHTML += getPokemon(result);
+    });
+}
+
+searchBar.addEventListener("keyup", searchPokemon);
+searchBar.addEventListener("keyup", showResult);
